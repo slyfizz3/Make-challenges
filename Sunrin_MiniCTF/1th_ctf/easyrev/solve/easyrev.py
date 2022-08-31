ddda77cae80273429c20f59f631b6b94572fb0c8
slyfizz3 <39259421+slyfizz3@users.noreply.github.com>
2022-08-31 23:27:09 +0900

    Delete Sunrin_MiniCTF directory

--- Sunrin_MiniCTF/1th_ctf/easyrev/solve/easyrev.py
+++
@@ -1,7 +0,0 @@
-form="Sunrin{lets_start_reversing}"
-form=list(map(ord,form))
-array=[]
-for i in range(len(form)):
-	array.append(form[i]^(0x23+i))
-print(array)
-print(len(form))
--- Sunrin_MiniCTF/2th_ctf/crackme/solve/solve.py
+++
@@ -1,6 +0,0 @@
-import string,base64
-table="0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasd+/=fghjklzxcvbnm"
-orige=string.ascii_uppercase+string.ascii_lowercase+string.digits+"+/="
-tb=string.maketrans(table,orige)
-flag=base64.b64decode("AjSaLewaXhRgRFUdKSvNR7E=F=GfFhSaG=1qHSvqJDICXDvgFjS=HSveLewqR3vv".translate(tb))
-print(flag)
